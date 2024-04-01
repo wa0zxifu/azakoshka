8cb7b2019604d9de8ff78477e4caf94e7a3e9c87
Ахметзянов Тимур Айратович <022202@mck-ktits.ru>
2024-04-01 13:50:16 +0300

    Initial commit

---
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -0,0 +1,36 @@
+package org.example.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "azacot_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6991360076:AAH9EUq67z4fiE37g4i3MCxTKv4LbAT9DGE";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
